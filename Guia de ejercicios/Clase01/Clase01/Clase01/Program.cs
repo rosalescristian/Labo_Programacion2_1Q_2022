139012d255e2c199b474cec9ab1a285f98a1a491
rosalescristian <rosalescristianm@gmnail.com>
2022-03-23 21:07:23 -0300

    Clase 01 - Programa 01 - 23/03/2021

---
+++ Guia de ejercicios/Clase01/Clase01/Clase01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Clase01
+{
+    internal class  Program
+    {               
+        static void Main(string[] args)
+        {
+            Console.Title="Clase 01 - Programa 01 - 23/03/2022";
+
+            int i = 0;
+            i++;
+
+            Console.WriteLine(i.ToString());
+
+
+            if(CheckArray(args))
+            {
+                Console.WriteLine("Hello World!" + args[0]);
+            }
+                
+            
+            Console.ReadKey();
+        }
+
+        static bool CheckArray(string [] args)
+        {
+            return args.Length > 0;
+        }
+    }
+}
