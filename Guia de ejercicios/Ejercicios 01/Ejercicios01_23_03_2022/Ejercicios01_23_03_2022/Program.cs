76b90d1d796fbff02f0cb2ee280ade809bca4162
rosalescristian <rosalescristianm@gmnail.com>
2022-03-23 21:51:31 -0300

    Ejercicio I01 Resuelto - 23/03/2022

---
+++ Guia de ejercicios/Ejercicios 01/Ejercicios01_23_03_2022/Ejercicios01_23_03_2022/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ejercicios01_23_03_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio I01 - Min, Max y Avg";
+
+            int numero, minimo=int.MaxValue, maximo=int.MinValue, suma=0, contador=0, promedio=0;
+
+            do
+            {
+                Console.WriteLine("Ingrese un nro Natural.");
+                if(int.TryParse(Console.ReadLine(), out numero)&&(numero >0 && numero <10))
+                {
+                    if (numero > maximo)
+                    {
+                        maximo = numero;
+                    }
+                    if (numero < minimo)
+                    {
+                        minimo = numero;
+                    }
+                    suma += numero;
+                    contador++;
+                    //Console.WriteLine("Nro 1: {0}, Nro 2: {1}", suma, contador);
+                }
+            } while (contador < 5);
+            promedio = suma / 5;
+            Console.WriteLine("El Maximo es: {0}.\n El Minimo es: {1}.\n El Promedio es: {2}", maximo, minimo, promedio);
+            Console.ReadKey();
+        }
+    }
+}
