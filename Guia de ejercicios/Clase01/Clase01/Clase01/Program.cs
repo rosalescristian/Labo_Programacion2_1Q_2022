6b2cc917aa620c4fab2efecb5ee19de91f2030e7
rosalescristian <rosalescristianm@gmnail.com>
2022-03-23 21:29:19 -0300

    Clase 01 - Programa 01 - 23/03/2021

--- Guia de ejercicios/Clase01/Clase01/Clase01/Program.cs
+++ Guia de ejercicios/Clase01/Clase01/Clase01/Program.cs
@@ -8,18 +8,27 @@ namespace PrimerPograma
         {
             Console.Title="Clase 01 - Programa 01 - 23/03/2022";
 
+            //Math.Min()
+            //Console.
+            
+            /*
+             * Ctrl+Barra => Habilita el IntelliSence de nuevo
+             * F2 => Renombrar
+             * Shift+Alt+ArrowDown => Editar varias lineas juntas
+             * Recordar la ventana de Inmediate Window
+             * 
+             * */
+            
             int i = 0;
             i++;
 
             Console.WriteLine(i.ToString());
-
 
             /*if(CheckArray(args))
             {*/
                 Console.WriteLine("Hello World!"/* + args[0]*/);
             //}
                 
-            
             Console.ReadKey();
         }
 
