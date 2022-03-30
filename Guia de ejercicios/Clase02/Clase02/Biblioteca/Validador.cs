a35b46ec467625e1a1229dd97274653402ba9199
rosalescristian <rosalescristianm@gmnail.com>
2022-03-30 19:58:06 -0300

    Ejercicio I01 - Completo

---
+++ Guia de ejercicios/Clase02/Clase02/Biblioteca/Validador.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public static class Validador
+    {
+        /// <summary>
+        /// Valida que el valor ingresado se encuentre en un rango determinado
+        /// </summary>
+        /// <param name="valor">Dato a Validar</param>
+        /// <param name="min">Minimo Valor Incluido</param>
+        /// <param name="max">Maximo Valor Incluido</param>
+        /// <returns>Devuelve TRUE si el valor se encuentra dentro del rango</returns>
+        public static bool Validar(int valor, int min, int max)
+        {
+            bool validador = false;
+            if((valor >= min) && (valor <= max))
+            {
+                validador = true;
+            }
+            return validador;
+        }
+    }
+}
--- Guia de ejercicios/Clase02/Clase02/Clase02/Program.cs
+++ Guia de ejercicios/Clase02/Clase02/Clase02/Program.cs
@@ -9,11 +9,43 @@ namespace Clase02
         {
             Console.Title = "Clase 02: 30/03/22 - Metodos";
 
-            float temperaturaCelsius = 30;
-            
-            float temperaturaKelvin = ConversorDeTemperatura.ConvertirCelsiusAKelvin(temperaturaCelsius);
+            //float temperaturaCelsius = 30;
+            //float temperaturaKelvin = ConversorDeTemperatura.ConvertirCelsiusAKelvin(temperaturaCelsius);
+            //Console.WriteLine(temperaturaKelvin);
 
-            Console.WriteLine(temperaturaKelvin);
+            int numero;
+            int contador = 0;
+            int min = int.MaxValue;
+            int max = int.MinValue;
+
+            Console.WriteLine("Ingrese el Limite Superior: ");
+            if (int.TryParse(Console.ReadLine(), out max))
+            {
+                Console.WriteLine("Ingrese el Limite Inferior: ");
+                if (int.TryParse(Console.ReadLine(), out min))
+                {
+                    do
+                    {
+                        Console.WriteLine("Ingrese un Numero: ");
+                        if (int.TryParse(Console.ReadLine(), out numero))
+                        {
+                            if (Validador.Validar(numero, min, max))
+                            {
+                                Console.WriteLine("El nro se encuentra dentro del rango ingresado");
+                            }
+                            else
+                            {
+                                Console.WriteLine("El nro NO se encuentra dentro del rango ingresado");
+                            }
+                            contador++;
+                        }
+                    } while (contador < 5);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ha ingresado un dato erroneo");
+            }
 
             Console.ReadKey();
         }
