f0b77ee4e1ea4e6e8bd20e6ce2c86a8c4d88fa8a
rosalescristian <rosalescristianm@gmnail.com>
2022-03-30 20:00:34 -0300

    Ejercicio I04 - Commit Inicial

---
+++ Guia de ejercicios/Clase02/Clase02/Clase 02 - Ejercicio I04 - Calculadora/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clase_02___Ejercicio_I04___Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
