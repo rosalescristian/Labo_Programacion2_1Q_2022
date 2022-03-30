5c81299c671030d63cab81d2dc07f840277fdd2c
rosalescristian <rosalescristianm@gmnail.com>
2022-03-30 19:33:38 -0300

    Agregamos Proyecto de Biblioteca de Clases - Metodos Estaticos y Atributos de Clase

--- Guia de ejercicios/Clase02/Clase02/Biblioteca/ConversorDeTemperatura.cs
+++ Guia de ejercicios/Clase02/Clase02/Biblioteca/ConversorDeTemperatura.cs
@@ -2,14 +2,28 @@
 
 namespace Biblioteca
 {
-    public class ConversorDeTemperatura
+    public static class ConversorDeTemperatura // Si la clase es estática, todos sus miembros, metodos deben ser estaticos
     {
+        private const float ceroAbsoluto = 273.15f; // Atributo de clase. Las constantes no necesitan q les defina el STATIC adelante xq lo toma de ese modo automaticamente
+        
         /// <summary>
         /// 
         /// </summary>
+        /// <param name="temperaturaCelsius"></param>
+        /// <returns></returns>
         public static float ConvertirCelsiusAKelvin(float temperaturaCelsius)
         {
-            return temperaturaCelsius + 273.15f;
+            return temperaturaCelsius + ceroAbsoluto;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="temperaturaKelvin"></param>
+        /// <returns></returns>
+        public static float ConvertirKelvinACelsius(float temperaturaKelvin)
+        {
+            return temperaturaKelvin - ceroAbsoluto;
         }
     }
 }
