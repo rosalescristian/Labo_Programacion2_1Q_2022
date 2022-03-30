536be3b7d571cb0d5072caad58e388174c76d657
rosalescristian <rosalescristianm@gmnail.com>
2022-03-30 20:46:03 -0300

    Ejercicio A01 - Completo

---
+++ Guia de ejercicios/Clase02/Clase02/Biblioteca/Factorial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public static class Factorial
+    {
+        /// <summary>
+        /// Calcula el Factorial de un nro
+        /// </summary>
+        /// <param name="calcularFactorial">Numero sobre el cual se va a calcular el factorial</param>
+        /// <returns>Devuelve el factorial del nro ingresado</returns>
+        public static int CalcularFactorial(int calcularFactorial)
+        {
+            /* El factorial de un número es una operación que consiste en multiplicar 
+            * un numero “n” por todos los números enteros positivos que estén debajo de él,
+            * por ejemplo el factorial de 3 es el resultado de multiplicar 3 por 2 por 1.
+            */
+            int factorial = 1;
+            for(int i=1;i<calcularFactorial+1;i++)
+            {
+                factorial = factorial * i;
+            }
+            return factorial;
+        }
+    }
+}
---
+++ Guia de ejercicios/Clase02/Clase02/Clase 02 - Ejercicio A01 - Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Biblioteca;
+
+namespace Clase_02___Ejercicio_A01___Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Clase 02 : 30/03/22 - Ejercicio A01 Calculo de Factorial";
+
+            int numeroUno;
+            Console.WriteLine("Ingrese un nro: ");
+            if(int.TryParse(Console.ReadLine(), out numeroUno))
+            {
+                Console.WriteLine(Factorial.CalcularFactorial(numeroUno));
+            }
+            Console.ReadKey();
+        }
+    }
+}
