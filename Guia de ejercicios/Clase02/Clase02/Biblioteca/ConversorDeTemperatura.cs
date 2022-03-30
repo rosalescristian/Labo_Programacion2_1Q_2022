2625057ecf9474ec22c6b11f03247b239f9bef8e
rosalescristian <rosalescristianm@gmnail.com>
2022-03-30 19:14:26 -0300

    Agregamos Proyecto de Biblioteca de Clases

---
+++ Guia de ejercicios/Clase02/Clase02/Biblioteca/ConversorDeTemperatura.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class ConversorDeTemperatura
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static float ConvertirCelsiusAKelvin(float temperaturaCelsius)
+        {
+            return temperaturaCelsius + 273.15f;
+        }
+    }
+}
---
+++ Guia de ejercicios/Clase02/Clase02/Clase02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Biblioteca;
+
+namespace Clase02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Clase 02: 30/03/22 - Metodos";
+
+            float temperaturaCelsius = 30;
+            
+            float temperaturaKelvin = ConversorDeTemperatura.ConvertirCelsiusAKelvin(temperaturaCelsius);
+
+            Console.WriteLine(temperaturaKelvin);
+
+            Console.ReadKey();
+        }
+    }
+}
