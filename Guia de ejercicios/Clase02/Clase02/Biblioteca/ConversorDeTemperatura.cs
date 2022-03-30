a956c15a3490780f190f6b0157b56fb83702bcfb
rosalescristian <rosalescristianm@gmnail.com>
2022-03-30 19:39:43 -0300

    Agregamos Proyecto de Biblioteca de Clases - Metodos Estaticos y Atributos de Clase con su documentacion

--- Guia de ejercicios/Clase02/Clase02/Biblioteca/ConversorDeTemperatura.cs
+++ Guia de ejercicios/Clase02/Clase02/Biblioteca/ConversorDeTemperatura.cs
@@ -7,20 +7,20 @@ namespace Biblioteca
         private const float ceroAbsoluto = 273.15f; // Atributo de clase. Las constantes no necesitan q les defina el STATIC adelante xq lo toma de ese modo automaticamente
         
         /// <summary>
-        /// 
+        /// Convierte el valor recibido a Grados Kelvin
         /// </summary>
-        /// <param name="temperaturaCelsius"></param>
-        /// <returns></returns>
+        /// <param name="temperaturaCelsius">Valor recibido que indica la temperatura a Convertir</param>
+        /// <returns>Devuelve la temperatura en Grados Kelvin</returns>
         public static float ConvertirCelsiusAKelvin(float temperaturaCelsius)
         {
             return temperaturaCelsius + ceroAbsoluto;
         }
 
         /// <summary>
-        /// 
+        /// Convierte el valor recibido a Grados Celsius
         /// </summary>
-        /// <param name="temperaturaKelvin"></param>
-        /// <returns></returns>
+        /// <param name="temperaturaKelvin">Valor recibido que indica la temperatura a Convertir</param>
+        /// <returns>Devuelve la temperatura en Grados Celsius</returns>
         public static float ConvertirKelvinACelsius(float temperaturaKelvin)
         {
             return temperaturaKelvin - ceroAbsoluto;
