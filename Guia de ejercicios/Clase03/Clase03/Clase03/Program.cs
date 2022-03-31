0f8fdf71b06f9418ee5436881623e58d0def8079
rosalescristian <rosalescristianm@gmnail.com>
2022-03-31 19:47:38 -0300

    Clase 3 - Commit inicial

---
+++ Guia de ejercicios/Clase03/Clase03/Clase03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Biblioteca;
+
+namespace Clase03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Clase 03: 31/03/2022 - ";
+            
+            
+            Console.WriteLine("Hello World!");
+
+
+
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
