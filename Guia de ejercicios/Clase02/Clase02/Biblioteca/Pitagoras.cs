9ac67da5db99a52bd6b8640b9f23116b82a8c891
rosalescristian <rosalescristianm@gmnail.com>
2022-03-31 15:58:48 -0300

    Ejercicio I07 - Completo

---
+++ Guia de ejercicios/Clase02/Clase02/Biblioteca/Pitagoras.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public static class Pitagoras
+    {
+        /// <summary>
+        /// Metodo que calcula el valor de la hipotenusa de acuerdo al teorema de pitagoras
+        /// </summary>
+        /// <param name="baseTriangulo">Base del triangulo</param>
+        /// <param name="altura">Altura del Triangulo</param>
+        /// <returns>Devuelve el valor de la hipotenusa</returns>
+        public static double CalcularHipotenusa(int baseTriangulo, int altura)
+        {
+            return Math.Sqrt(Math.Pow(baseTriangulo,2)+ Math.Pow(altura,2));
+        }
+    }
+}
---
+++ Guia de ejercicios/Clase02/Clase02/Clase 02 - Ejercicio I07 - Pitagoras/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Biblioteca;
+
+namespace Clase_02___Ejercicio_I07___Pitagoras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Clase 02 : 30/03/2022 - Ejercicio I07";
+
+            int baseTriangulo, altura;
+
+            Console.WriteLine("Ingrese la base del triangulo: ");
+            if (int.TryParse(Console.ReadLine(), out baseTriangulo))
+            {
+                Console.WriteLine("Ingrese la altura del triangulo: ");
+                if (int.TryParse(Console.ReadLine(), out altura))
+                {
+                    Console.WriteLine("La hipotenusa del Triangulo es: {0}",Pitagoras.CalcularHipotenusa(baseTriangulo, altura));
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
