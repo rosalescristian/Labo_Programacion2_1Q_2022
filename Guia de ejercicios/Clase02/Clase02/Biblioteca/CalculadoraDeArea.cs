2f8ac8d89258505ab4d60871d026114314cc93ed
rosalescristian <rosalescristianm@gmnail.com>
2022-03-30 22:14:02 -0300

    Ejercicio I06 - Completado

---
+++ Guia de ejercicios/Clase02/Clase02/Biblioteca/CalculadoraDeArea.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public static class CalculadoraDeArea
+    {
+        /// <summary>
+        /// Calcula la superficie de un cuadrado
+        /// </summary>
+        /// <param name="longitudLado">Largo del lado </param>
+        /// <returns>Devuelve el valor cuadrado del lado</returns>
+        public static double CalcularAreaCuadrado(double longitudLado)
+        {
+            return longitudLado * longitudLado;
+        }
+
+        /// <summary>
+        /// Devuelve el area del Triangulo
+        /// </summary>
+        /// <param name="baseTriangulo">Largo de la base del triangulo</param>
+        /// <param name="altura">Altura del triangulo</param>
+        /// <returns>Devuelve la superficio del triangulo ingresado</returns>
+        public static double CalcularAreaTriangulo(double baseTriangulo, double altura)
+        {
+            return Math.Round((baseTriangulo * altura)/2,2,MidpointRounding.ToEven);
+        }
+
+        /// <summary>
+        /// Devuelve el area de un circulo
+        /// </summary>
+        /// <param name="radio">Radio del circulo</param>
+        /// <returns>Devuelve el area del circulo ingresado</returns>
+        public static double CalcularAreaCirculo(double radio)
+        {
+            return Math.Round(Math.PI * (radio * radio),2,MidpointRounding.ToEven);
+        }
+    }
+}
---
+++ Guia de ejercicios/Clase02/Clase02/Clase 02 - Ejercicio I06 - Areas/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Biblioteca;
+
+namespace Clase_02___Ejercicio_I06___Areas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Clase 02: 30/03/22 - Ejercicio I06";
+
+            int opcion;
+            double lado;
+            double altura;
+            double radio;
+            double areaCuadrado;
+            double areaTriangulo;
+            double areaCirculo;
+            char exit = 'n';
+
+            do
+            {
+                Console.WriteLine("Seleccione el calculo que desea resolver:\n\t1- Area Cuadrado\n\t2- Area Triangulo\n\t3- Area Circulo\n");
+                if (int.TryParse(Console.ReadLine(), out opcion))
+                {
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.WriteLine("Ingrese el largo del lado del cuadrado: ");
+                            if (double.TryParse(Console.ReadLine(), out lado))
+                            {
+                                areaCuadrado = CalculadoraDeArea.CalcularAreaCuadrado(lado);
+                                Console.WriteLine("El area del cuadrado es {0} cm cuadrados.", areaCuadrado);
+                            }
+                            break;
+                        case 2:
+                            Console.WriteLine("Ingrese la base del triangulo: ");
+                            if (double.TryParse(Console.ReadLine(), out lado))
+                            {
+                                Console.WriteLine("Ingrese la altura del triangulo: ");
+                                if (double.TryParse(Console.ReadLine(), out altura))
+                                {
+                                    areaTriangulo = CalculadoraDeArea.CalcularAreaTriangulo(lado, altura);
+                                    Console.WriteLine("El area del triangulo es {0} cm cuadrados.", areaTriangulo);
+                                }
+                            }
+                            break;
+                        case 3:
+                            Console.WriteLine("Ingrese el radio del circulo: ");
+                            if (double.TryParse(Console.ReadLine(), out radio))
+                            {
+                                areaCirculo = CalculadoraDeArea.CalcularAreaCirculo(radio);
+                                Console.WriteLine("El area del circulo es {0} cm cuadrados.", areaCirculo);
+                            }
+                            break;
+                    }
+                    Console.WriteLine("Desea salir? (s/n)\n");
+                    char.TryParse(Console.ReadLine(), out exit);
+                }
+            } while (exit == 'n');
+            
+            Console.ReadKey();
+        }
+    }
+}
