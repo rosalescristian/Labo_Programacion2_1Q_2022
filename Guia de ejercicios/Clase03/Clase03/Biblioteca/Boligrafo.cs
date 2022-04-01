12353b7d70d5f4c281b52a04ac56c24e940eb9b0
rosalescristian <rosalescristianm@gmnail.com>
2022-04-01 12:59:51 -0300

    Clase 04: Ejercicio I04 - Metodo Recargar Completo

---
+++ Guia de ejercicios/Clase03/Clase03/Biblioteca/Boligrafo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Boligrafo
+    {
+        #region Atributos
+        public const short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+        #endregion
+
+        /// <summary>
+        /// Consrtuctor
+        /// </summary>
+        /// <param name="tinta"></param>
+        /// <param name="color"></param>
+        public Boligrafo(short tinta, ConsoleColor color)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public short GetTinta()
+        {
+            return this.tinta;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="gasto"></param>
+        /// <param name="dibujo"></param>
+        /// <returns></returns>
+        public bool Pintar(short gasto, out string dibujo)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Recargar()
+        {
+            short recarga = (short)(100-GetTinta());
+            SetTinta(recarga);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tinta"></param>
+        private void SetTinta(short tinta)
+        {
+            short nivelTinta = GetTinta();
+            int nuevoNivelTinta = nivelTinta + tinta;
+            if(nuevoNivelTinta > -1 && nuevoNivelTinta < 101)
+            {
+                this.tinta += tinta;
+            }
+
+        }
+
+
+    }
+}
