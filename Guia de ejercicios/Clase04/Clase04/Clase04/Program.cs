79ac9c1c9c7f1788623ee22142750e0eb0313fa3
rosalescristian <rosalescristianm@gmnail.com>
2022-04-06 20:15:11 -0300

    Clase 04: Commit Inicial

---
+++ Guia de ejercicios/Clase04/Clase04/Clase04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Clase04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Clase 04 : 06/04/2022 - Sobrecargas";
+
+            Console.WriteLine("Hello World!");
+            
+            Console.ReadKey();
+        }
+    }
+}
