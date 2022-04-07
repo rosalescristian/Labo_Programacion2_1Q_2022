8732d2557b493ea99830a89ae8b16b47ddfb0820
rosalescristian <rosalescristianm@gmnail.com>
2022-04-06 21:28:04 -0300

    Ejercicio I01: Sumador - COMPLETO

--- Guia de ejercicios/Clase04/Clase04/Biblioteca/Sumador.cs
+++ Guia de ejercicios/Clase04/Clase04/Biblioteca/Sumador.cs
@@ -4,5 +4,79 @@ namespace Biblioteca
 {
     public class Sumador
     {
+        private int cantidadSumas;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Sumador():this(0) // :this(0) es para reutilizar el otro Constructor
+        {
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cantidadSumas"></param>
+        public Sumador(int cantidadSumas)
+        {
+            this.cantidadSumas = cantidadSumas;
+        }
+
+        /// <summary>
+        /// Conversion Expicita a INT
+        /// </summary>
+        /// <param name="s"></param>
+        public static explicit operator int(Sumador s)
+        {
+            return s.cantidadSumas;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2"></param>
+        /// <returns></returns>
+        public static bool operator |(Sumador s1, Sumador s2)
+        {
+            return s1.cantidadSumas == s2.cantidadSumas;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2"></param>
+        /// <returns></returns>
+        public static long operator +(Sumador s1, Sumador s2)
+        {
+            return s1.cantidadSumas + s2.cantidadSumas;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public long Sumar(long a, long b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public string Sumar(string a, string b)
+        {
+            this.cantidadSumas++;
+            return a + b; // Concatena Strings
+        }
     }
 }
---
+++ Guia de ejercicios/Clase04/Clase04/Clase04/Ej I01 - Sumador.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Biblioteca;
+
+namespace Clase04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Clase 04 : 06/04/2022 - Sobrecargas; Ejercicio I01: Sumador";
+
+            Sumador sumadorUno = new Sumador();
+            Sumador sumadorDos = new Sumador(15);
+
+            long resultadoLongUno = sumadorUno.Sumar(4, 5);
+            Console.WriteLine($"Cantidad de Sumas = {(int)sumadorUno}");
+
+            string resultadoStringUno = sumadorUno.Sumar("Hola", " Mundo");
+
+            Console.WriteLine($"Resultado de long + long = {resultadoLongUno}"); // {Environment.NewLine} Funciona en los Console.WriteLine
+
+            Console.WriteLine($"Cantidad de Sumas = {(int)sumadorUno}");
+
+            Console.WriteLine($"Resultado de string + string = {resultadoStringUno}");
+
+            Console.WriteLine($"Cantidad de Sumas = {(int)sumadorUno}");
+
+            Console.WriteLine($"Cantidad de Sumas s1 + s2={sumadorUno+sumadorDos}");
+
+            Console.WriteLine($"Son iguales si tienen misma cantidad de sumas = {sumadorUno | sumadorDos}");
+
+            Console.ReadKey();
+        }
+    }
+}
