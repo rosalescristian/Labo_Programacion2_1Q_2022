bee8c57380bbcfffa6a1430e8a0e7dc8b29e9104
rosalescristian <rosalescristianm@gmnail.com>
2022-04-28 21:29:54 -0300

    Clases y Metodos Abstractos - Sume Clase ANIMALABSTRACTA y PERROABS con sus metodos heredados y con OVERRIDE entre ellos

---
+++ Guia de ejercicios/Clase09/Consola/Biblioteca/AnimalAbstracta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public abstract class AnimalAbstracta
+    {
+        public string nombre;
+
+        public AnimalAbstracta(string nombre)// El constructor se mantiene porq puede tener atributos
+        {
+            this.nombre = nombre;
+        }
+
+        public abstract string JuegoFavorito { get; }
+
+        public abstract string EmitirSonido();
+
+        public string Pensar()
+        {
+            return "Pensando...";
+        }
+        public static bool operator ==(Animal g1, Animal g2)
+        {
+            return g1 is not null && g2 is not null && g1.nombre == g2.nombre;
+        }
+
+        public static bool operator !=(Animal g1, Animal g2)
+        {
+            return !(g1 == g2);
+        }
+    }
+}
---
+++ Guia de ejercicios/Clase09/Consola/Biblioteca/PerroAbs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class PerroAbs : AnimalAbstracta
+    {
+        public PerroAbs(string nombre):base(nombre)
+        {
+
+        }
+
+        public override string JuegoFavorito
+        {
+            get
+            {
+                return "pelota";
+            }
+        }
+
+        public override string EmitirSonido()
+        {
+            return $"Woof Gau Abstracto";
+        }
+    }
+}
