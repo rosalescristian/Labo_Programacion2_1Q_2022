26851633259456cbce683351eade29182491e614
rosalescristian <rosalescristianm@gmnail.com>
2022-04-30 16:48:11 -0300

    Clase 06: Colecciones - Revision

---
+++ Guia de ejercicios/Clase06/Clase06/Clase06/Clase06/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase06
+{
+    public class Persona
+    {
+        string nombre;
+
+        public Persona(string nombre)
+        {
+            this.nombre = nombre;
+        }
+    }
+}
---
+++ Guia de ejercicios/Clase06/Clase06/Clase06/Clase06/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Clase06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Main con Regiones, OJO
+
+            #region List - GENERICA
+
+            List<string> mascotas = new List<string>();
+            mascotas.Add("Firulais");
+            mascotas.Add("Salem");
+            mascotas.Add("Bola de nieve");
+            mascotas.Add("Gaturro");
+
+            Console.WriteLine($"La mnascota de la posicion 3 es: {mascotas[3]}");
+            
+            Console.WriteLine("Recorro con ForEach");
+            foreach(string mascota in mascotas)
+            {
+                Console.WriteLine(mascota);
+            }
+
+            #endregion
+
+            #region ArrayList - NO GENERICA - Esta no la usamos
+            //Estas colecciones pueden recibir cualquier tipo de dato, x eso tiene pesona y booleano
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(15);
+            arrayList.Add(new Persona("Esteban"));
+            arrayList.Add(false);
+
+            #endregion
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
