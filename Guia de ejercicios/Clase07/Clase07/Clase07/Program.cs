c7473f88f894d6420de7fe9b594419e9b0126dda
rosalescristian <rosalescristianm@gmnail.com>
2022-04-30 17:44:21 -0300

    Clase06: Sort Asc y Desc Final. Commit Inicial Clase 07: Encapsulamiento

---
+++ Guia de ejercicios/Clase07/Clase07/Clase07/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Clase07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
