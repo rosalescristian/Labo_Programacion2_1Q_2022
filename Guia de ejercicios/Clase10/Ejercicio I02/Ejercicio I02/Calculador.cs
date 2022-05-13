2fd963d54f84f55edbc2fcd47b6d81faea137ee2
rosalescristian <rosalescristianm@gmnail.com>
2022-05-12 21:15:56 -0300

    Ejercicio I02 - Atrapame si puedes resuelto. Commentario Agregado al Parcial 1

---
+++ Guia de ejercicios/Clase10/Ejercicio I02/Ejercicio I02/Calculador.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_I02
+{
+    public static class Calculador
+    {
+        public static int Calcular(int valorUno, int valorDos)
+        {
+            int cociente;
+            try
+            {
+                cociente = valorUno / valorDos;
+            }
+            catch (DivideByZeroException)
+            {
+                cociente = int.MinValue;
+                Console.WriteLine("ERROR! No se puede dividir por cero");
+            }
+            return cociente;
+
+
+        }
+
+    }
+}
---
+++ Guia de ejercicios/Clase10/Ejercicio I02/Ejercicio I02/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_I02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(String.IsNullOrEmpty(txtKilometros.Text) || String.IsNullOrEmpty(txtLitros.Text))
+                {
+                    throw new ParametrosVaciosException();
+                }
+                try
+                {
+                    int km = int.Parse(txtKilometros.Text);
+                    int lts = int.Parse(txtLitros.Text);
+                    try
+                    {
+                        int conversion = Calculador.Calcular(km, lts);
+                        if (conversion == int.MinValue)
+                        {
+                            throw new DivideByZeroException();
+                        }
+                        rtbConversion.Text = $"Valor convertido: {conversion.ToString()}";
+                    }
+                    catch (DivideByZeroException)
+                    {
+
+                        MessageBox.Show("No se puede dividir por CERO", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); ;
+                    }
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("No se puede convertir este valor", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch(OverflowException)
+                {
+                    MessageBox.Show("Valor fuera de rango", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (ParametrosVaciosException)
+            {
+                MessageBox.Show("No se pueden dejar los campos de texto vacios", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
