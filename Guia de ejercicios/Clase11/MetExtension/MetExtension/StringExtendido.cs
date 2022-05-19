a6b0101144420c852397dbbd0f414ecaf2e142eb
rosalescristian <rosalescristianm@gmnail.com>
2022-05-19 19:04:51 -0300

    Commit Inicial clase 10

---
+++ Guia de ejercicios/Clase11/MetExtension/MetExtension/StringExtendido.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MetExtension
+{
+    public static class StringExtendido
+    {
+
+        public static int ContarPalabras(this String s)
+        {
+
+        }
+    }
+}
