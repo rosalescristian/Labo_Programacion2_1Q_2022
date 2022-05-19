305f6d1155ac0e78e52aca0ea85dee3d0b9ad65e
rosalescristian <rosalescristianm@gmnail.com>
2022-05-19 19:29:42 -0300

    Ejercicio FizzBuzz Terminado

---
+++ Guia de ejercicios/Clase11/FizzBuzz/Consola/Biblioteca/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Biblioteca
+{
+    public static class FizzBuzz
+    {
+        public static string Dividir(this Int32 i)
+        {
+            string retorno = string.Empty; //Esta fue la inicializacion de Esteban
+            
+            if (i % 3 == 0)
+            {
+                retorno += "Fizz";
+            }
+            if(i%5 == 0)
+            {
+                retorno += "Buzz";
+            }
+            if (string.IsNullOrEmpty(retorno))
+            {
+                retorno = i.ToString(); 
+            }
+
+            return retorno;
+        }
+    }
+}
---
+++ Guia de ejercicios/Clase11/FizzBuzz/Consola/Consola/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Biblioteca;
+
+namespace Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char salir = 'N';
+            int numero;
+            do
+            {
+                Console.WriteLine("Ingrese un nro: ");
+                if(int.TryParse(Console.ReadLine(), out numero))
+                {
+                    Console.WriteLine(numero.Dividir());
+                }
+                Console.WriteLine("Desea salir? S/N");
+                Char.TryParse(Console.ReadLine(), out salir);
+               
+            } while (salir == 'N');
+        }
+    }
+}
