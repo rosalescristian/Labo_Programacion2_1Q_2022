f831485769f0f672451bd7cf71c9fa5980ecc281
rosalescristian <rosalescristianm@gmnail.com>
2022-06-01 20:44:10 -0300

    Commit Final ManejadorArchivos (Read y Write)

--- Guia de ejercicios/Clase13/Clase_Archivos_Serializacion/Consola/Program.cs
+++ Guia de ejercicios/Clase13/Clase_Archivos_Serializacion/Consola/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using GestorDeArchivos;
+using static System.Environment; // forma de incluir una clase que despues es parte del scope del archivo
+// using static System.Console; // Tb podriamos hacer esto con Console y no incluirlo en las lineas de codigo antes del WriteLine por ejemplo
+using System.IO;
+using System.Text;
 
 namespace Consola
 {
@@ -6,7 +11,27 @@ namespace Consola
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = GetFolderPath(SpecialFolder.Desktop) + @"/Archivos/MiArchivo.txt";
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Habia una vez una vaca");
+            sb.AppendLine("en la quebrada de humahuaca");
+            sb.AppendLine("Maria Elena Walsh");
+
+            try
+            {
+                //ManejadorArchivosDeTexto.Guardar(path, sb.ToString(),true);
+                ManejadorArchivosDeTexto.Guardar(path, sb.ToString(),false);
+
+                Console.WriteLine(ManejadorArchivosDeTexto.Leer(path));
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
         }
     }
 }
--- Guia de ejercicios/Clase13/Clase_Archivos_Serializacion/GestorDeArchivos/ManejadorArchivosDeTexto.cs
+++ Guia de ejercicios/Clase13/Clase_Archivos_Serializacion/GestorDeArchivos/ManejadorArchivosDeTexto.cs
@@ -1,8 +1,76 @@
 ﻿using System;
+using System.IO;
 
 namespace GestorDeArchivos
 {
-    public class Class1
+    public class ManejadorArchivosDeTexto
     {
+        public static void Guardar(string path, string texto)
+        {
+            StreamWriter streamWriter = null;
+            try
+            {
+                streamWriter = new StreamWriter(path);//el path donde se guarda el archivo
+                streamWriter.WriteLine(texto); //este metodo guarda el string TEXTO dentro del path
+            
+                streamWriter.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if (streamWriter is not null) //Una forma de gestionarlo
+                {
+                    streamWriter.Close();
+                }
+            }
+
+
+        }
+
+        /// <summary>
+        /// Sobrecarga de Guardar agregando info al archivo existente
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="texto"></param>
+        /// <param name="agregar"></param>
+        public static void Guardar(string path, string texto, bool agregar)
+        {
+            try
+            {
+                using (StreamWriter streamWriter = new StreamWriter(path, agregar))// otra forma de manejar la escritura con el cierre del archivo incluido y no tenemos q hacer el Close()
+                {
+                    streamWriter.WriteLine(texto);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public static string Leer(string path)
+        {
+            try
+            {
+                using(StreamReader streamReader = new StreamReader(path))
+                {
+                    return streamReader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+
+
     }
 }
